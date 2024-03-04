eae51ad5d453d497d216b71ac44551c6fdd8d94c
Genesis Marvin <coolbuddha56@gmail.com>
2024-03-04 23:35:02 +0800

    Fixed all href - konting ayos nalang sa design

---
+++ JAVASCRIPT/SCRIPTS.js
@@ -0,0 +1,15 @@
+swal({
+	title: "Good job!",
+	text: "You clicked the button!",
+	icon: "success",
+	buttons: {
+		confirm: {
+			text: "Confirm Me",
+			value: true,
+			visible: true,
+			className: "btn btn-success",
+			closeModal: true
+		}
+	}
+});
+
