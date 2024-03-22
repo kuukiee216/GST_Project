9730e06beabd820ff44bfd573525128fcc5dcd5a
Genesis Marvin <coolbuddha56@gmail.com>
2024-03-23 03:09:47 +0800

    Changed design - made some dynamic

--- applicant/SignIn.php
+++ applicant/SignIn.php
@@ -3,20 +3,29 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Required meta tags -->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport'>
 
-   <!-- Bootstrap CSS -->
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
-   rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-   <link href="../CSS/SignIn.css" rel="stylesheet">
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-   <link rel="preconnect" href="https://fonts.googleapis.com">
-   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-   <link href="https://fonts.googleapis.com/css2?family=Road+Rage&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <!-- CSS Files -->
+	  <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+	  <link rel="stylesheet" href="../assets/css/atlantis.min.css">
+    <link href="/CSS/applicant.css" rel="stylesheet">
+
+    <!-- Fonts and icons -->
+    <script src="../assets/js/plugin/webfont/webfont.min.js"></script>
+    <script>
+      WebFont.load({
+        google: {"families":["Lato:300,400,700,900"]},
+        custom: {"families":["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands", "simple-line-icons"], urls: ['../assets/css/fonts.min.css']},
+        active: function() {
+          sessionStorage.fonts = true;
+        }
+      });
+    </script>
 
     <title>Sign in form</title>
+    <link rel="icon" type="image/x-icon" href="/assets/img/jj_logo.png">
   </head>
 
   <body>
@@ -26,7 +35,7 @@
     <nav class="navbar navbar-expand-sm">
       <div class="container-fluid">
         <a class="navbar-brand font-RR text-white" href="/FILES-Applicant Side/Landing_Page.html">
-        <img src="../assets/img/japanLogo.png" alt="logo" style="width:80px;"> Japan Jobs</a>
+        <img src="../assets/img/JapanJobs.png" alt="logo" style="width:80px;"></a>
       </div>
     </nav>
         
@@ -81,32 +90,90 @@
         </form>
       </div>
 
-    <!--bottom navbar-->
-    <footer class="p-2 navbar">
-      
-      <ul class="nav">
-          <li class="nav-item">
-            <a class="form-label nav-link text-white" href="#">Privacy</a>
-          </li>
-
-          <li class="nav-item">
-            <a class="form-label nav-link text-white" href="#">Terms & Condition</a>
-          </li>
-
-          <li class="nav-item">
-            <a class="form-label nav-link text-white" href="#">Protect yourself online</a>
-          </li>
-
-          <li class="nav-item">
-            <a class="form-label nav-link text-white" href="#">Contact</a>
-          </li>
-          <a class="nav-link disabled text-dark">© 2024 JAPAN JOBS.All rights reserved</a>
-        </ul>
-      </div>
+        <!--bottom navbar-->
+        <footer class="footer fixed-bottom text-white" style="background-color:mediumseagreen">
+        <div class="container-fluid">
+            <nav class="pull-left">
+                <ul class="nav">
+                    <li class="nav-item">
+                        <a class="nav-link">
+                            Privacy
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">
+                            Terms & Condition
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">
+                            Protect yourself online
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="ml-auto">
+                © 2024 JAPAN JOBS.All rights reserved by Japan Jobs
+            </div>				
+        </div>
     </footer>
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js" integrity="sha512-ykZ1QQr0Jy/4ZkvKuqWn4iF3lqPZyij9iRv6sGqLRdTPkY69YX6+7wvVGmsdBbiIfN/8OdsI7HABjvEok6ZopQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <!-- Option 1: Bootstrap scripts -->
+        <script src="../.../assets/js/atlantis.js"></script>
+        <script src="../../assets/js/plugin/webfont/webfont.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js" integrity="sha512-ykZ1QQr0Jy/4ZkvKuqWn4iF3lqPZyij9iRv6sGqLRdTPkY69YX6+7wvVGmsdBbiIfN/8OdsI7HABjvEok6ZopQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+        <!-- Vendor JS Files -->
+        <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="../assets/vendor/glightbox/js/glightbox.min.js"></script>
+        <script src="../assets/vendor/purecounter/purecounter_vanilla.js"></script>
+        <script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+        <script src="../assets/vendor/swiper/swiper-bundle.min.js"></script>
+        <script src="../assets/vendor/aos/aos.js"></script>
+        <script src="../assets/vendor/php-email-form/validate.js"></script>
+
+        <!--   Core JS Files   -->
+        <script src="../assets/js/core/jquery.3.2.1.min.js"></script>
+        <script src="../assets/js/core/popper.min.js"></script>
+        <script src="../assets/js/core/bootstrap.min.js"></script>
+
+        <!-- jQuery UI -->
+        <script src="../assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
+        <script src="../assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js"></script>
+
+        <!-- jQuery Scrollbar -->
+        <script src="../assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
+
+
+        <!-- Chart JS -->
+        <script src="../assets/js/plugin/chart.js/chart.min.js"></script>
+
+        <!-- jQuery Sparkline -->
+        <script src="../assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js"></script>
+
+        <!-- Chart Circle -->
+        <script src="../assets/js/plugin/chart-circle/circles.min.js"></script>
+
+        <!-- Datatables -->
+        <script src="../assets/js/plugin/datatables/datatables.min.js"></script>
+
+        <!-- Bootstrap Notify -->
+        <script src="../assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js"></script>
+
+        <!-- jQuery Vector Maps -->
+        <script src="../assets/js/plugin/jqvmap/jquery.vmap.min.js"></script>
+        <script src="../assets/js/plugin/jqvmap/maps/jquery.vmap.world.js"></script>
+
+        <!-- Sweet Alert -->
+        <script src="../assets/js/plugin/sweetalert/sweetalert.min.js"></script>
+
+        <!-- Atlantis JS -->
+        <script src="../assets/js/atlantis.min.js"></script>
   </body>
 </html>
--- applicant/almost_done.php
+++ applicant/almost_done.php
@@ -1,33 +1,52 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport'>
 
-   <!-- Bootstrap CSS -->
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
-   rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-   <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:300,400,700,900" media="all">
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-   <link rel="preconnect" href="https://fonts.googleapis.com">
-   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-   <link href="/assets/css/atlantis.css" rel="stylesheet">
-   <link href="/CSS/almost_done.css" rel="stylesheet">
-   <link href="https://fonts.googleapis.com/css2?family=Road+Rage&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <!-- CSS Files -->
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/atlantis.min.css">
+    <link href="/applicant/applicant.css" rel="stylesheet">
+
+    <!-- Fonts and icons -->
+    <script src="../assets/js/plugin/webfont/webfont.min.js"></script>
+    <script>
+      WebFont.load({
+        google: {"families":["Lato:300,400,700,900"]},
+        custom: {"families":["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands", "simple-line-icons"], urls: ['../assets/css/fonts.min.css']},
+        active: function() {
+          sessionStorage.fonts = true;
+        }
+      });
+      </script>
 
     <title>Almost Done</title>
     <link rel="icon" type="image/x-icon" href="/assets/img/jj_logo.png">
   </head>
 
   <body>
-    <!-- Japan job posting icon href-->
-    <nav class="navbar navbar-expand-sm">
+    <!-- Navbar Header -->
+    <nav class="navbar navbar-header navbar-expand-lg" data-background-color="green2">
       <div class="container-fluid">
-        <a class="navbar-brand font-RR text-white" href="/FILES-Applicant Side/Landing_Page.html">
-        <img src="/assets/img/jj_logo.png" alt="logo" style="width:80px;"> Japan Jobs</a>
-      </div>
+        <div class="collapse" id="search-nav">
+          <div>
+          <form class="navbar-left navbar-form">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar nav me-auto mb-2 mb-lg-0">
+               <li>
+                   <a href="/applicant/Landing_Page.html" class="logo">
+                     <img src="../assets/img/JapanJobs.png" alt="navbar brand">
+                   </a>
+               </li>
+              </ul>
+            </div>
+          </form>
+          </div>
+        </div>
+        </div>
     </nav>
+    <!-- End Navbar -->
         
         <div class="container flex justify-content-center">
             <form class="mx-auto was-validated">
@@ -69,26 +88,35 @@
     </div>
 
     <!--bottom navbar-->
-    <footer class="p-2 navbar">
-      
-      <ul class="nav">
-          <li class="nav-item">
-            <a class="form-label nav-link text-white" href="#">Privacy</a>
-          </li>
-
-          <li class="nav-item">
-            <a class="form-label nav-link text-white" href="#">Terms & Condition</a>
-          </li>
-
-          <li class="nav-item">
-            <a class="form-label nav-link text-white" href="#">Protect yourself online</a>
-          </li>
-
-          <li class="nav-item">
-            <a class="form-label nav-link text-white" href="#">Contact</a>
-          </li>
-          <a class="nav-link disabled text-dark">© 2024 JAPAN JOBS.All rights reserved</a>
-        </ul>
+    <footer class="footer text-white" style="background-color:mediumseagreen">
+      <div class="container-fluid">
+          <nav class="pull-left">
+              <ul class="nav">
+                  <li class="nav-item">
+                      <a class="nav-link">
+                          Privacy
+                      </a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link">
+                          Terms & Condition
+                      </a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link">
+                          Protect yourself online
+                      </a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link">
+                          Contact
+                      </a>
+                  </li>
+              </ul>
+          </nav>
+          <div class="ml-auto">
+              © 2024 JAPAN JOBS.All rights reserved by Japan Jobs
+          </div>				
       </div>
     </footer>
 
